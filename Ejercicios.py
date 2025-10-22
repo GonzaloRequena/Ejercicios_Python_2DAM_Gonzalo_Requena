c43e905380250182ac1c35aee6d98a63c54a9ca0
Gonzalo Requena <gonzalo.re8@gmail.com>
2025-10-22 10:31:33 +0200

    Initial commit

---
+++ Ejercicios.py
@@ -0,0 +1,99 @@
+# #EP0001
+# print("Soy un programa en Python")
+# print("\n")
+
+# #EP0002
+# print(5 + 3)
+# print(10 - 4)
+# print(6 * 7)
+# print(20 / 5)
+# print(10 % 3)
+# print((5 + 3) * 2)
+# print("\n")
+
+# #EP0003
+# print(-8 + 6 * 3)
+# print((2 + 7) % 5)
+# print(2 + (-1 * 8) - 5 % 3)
+# print("\n")
+
+# #EP0004
+# a = -8
+# b = 6
+# c = 3
+# d = 2
+# e = 7
+# f = 5
+# g = -1
+# h = 8
+# resultado1 = a + b * c #-8 + 6 * 3
+# resultado2 = (d + e) % f #(2 + 7) % 5
+# resultado3 = d + (g * h) - f % c #2 + ( -1 * 8 ) -5 % 3
+# print("Resultado de -8 + 6 * 3 = " + str(resultado1))
+# print("Resultado de (2 + 7) % 5 = " + str(resultado2))
+# print("Resultado de 2 + ( -1 * 8 ) -5 % 3 = " + str(resultado3))
+# print("\n")
+
+# #EP0005 -- No se puede hacer, da error
+
+# #EP0006
+# entero1 = 321
+# entero2 = 698
+# resultado = entero1 / entero2
+# print("El resultado de " + str(entero1) + " entre " + str(entero2) + " es: " + str(round(resultado, 3)))
+# print("\n")
+
+# #EP0012
+# numA = 129070
+# numB = 20689
+# resultado = numA * numB
+# print(resultado)
+# print("\n")
+
+# #EP0085
+# a = 3
+# a += 1
+# b = a
+# c = a
+# a += 1
+# b = b * 3
+# a = a * 2
+# print("a = " + str(a) + ", b = " + str(b) + ", c = " + str(c))
+# print("\n")
+
+# #EP0007
+# nombre = str(input("Dame tu nombre: "))
+# print("Hola, " + nombre)
+# print("\n")
+
+# #EP008
+# a, b = int(input("Dime un número: ")), int(input("Dime un número: "))
+# print("a + b = ", a + b)
+# print("\n")
+
+# #EP0009
+# metros = int(input("Dime una cantidad de metros: "))
+# millas = metros / 1609
+# print("La cantidad en millas es: ", millas)
+# print("\n")
+
+# #EP0010
+# a, b = int(input("Dime un número: ")), int(input("Dime un número: "))
+# print(a, " entre ", b, " es igual a ", a / b, ", con resto ", a % b)
+# print("\n")
+
+# #EP0011
+# numero = int(input("Dime un número: "))
+# print(numero, "x 1 = ", numero * 1)
+# print(numero, "x 2 = ", numero * 2)
+# print(numero, "x 3 = ", numero * 3)
+# print(numero, "x 4 = ", numero * 4)
+# print(numero, "x 5 = ", numero * 5)
+# print(numero, "x 6 = ", numero * 6)
+# print(numero, "x 7 = ", numero * 7)
+# print(numero, "x 8 = ", numero * 8)
+# print(numero, "x 9 = ", numero * 9)
+# print(numero, "x 10 = ", numero * 10)
+# print("\n")
+
+# #EP0059
